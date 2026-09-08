be8acbb9aaa1f82f4cbe375c8bc0abe778e7d257
waxew <4622.ar@gmail.com>
2026-09-08 22:19:33 +0330

    Remove Java Activity after Kotlin migration

--- app/src/main/java/com/example/hi/MainActivity.java
+++
@@ -1,3 +0,0 @@
-package com.example.hi;
-import android.app.Activity; import android.os.Bundle; import android.graphics.Color; import android.view.Gravity; import android.widget.TextView;
-public class MainActivity extends Activity { public void onCreate(Bundle s){super.onCreate(s); TextView t=new TextView(this); t.setText("سلام"); t.setTextSize(32); t.setTextColor(Color.BLACK); t.setGravity(Gravity.CENTER); setContentView(t);} }
